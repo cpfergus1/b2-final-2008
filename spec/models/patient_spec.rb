a4318c9b154309612280b5b6dc14ad987f2c0ce1
Connor Ferguson <cpfergus1@gmail.com>
2020-11-09 11:17:50 -0700

    built tests for order by age method

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Patient, type: :model do
   describe "Relations" do
     it {should have_many(:doctors).through(:patients_doctors)}
   end
+
+  describe 'class methods' do
+    before :each do
+      @patient_1 = Patient.create!(name: 'Katie Bryce',
+                                    age: 24)
+      @patient_2 = Patient.create!(name: 'Denny Duquette',
+                                    age: 39)
+      @patient_3 = Patient.create!(name: 'Rebecca Pope',
+                                    age: 32)
+    end
+    it '.ordered_by_age' do
+      expect(Patient.ordered_by_age.first).to eq(@patient_2)
+      expect(Patient.ordered_by_age.second).to eq(@patient_3)
+      expect(Patient.ordered_by_age.last).to eq(@patient_1)
+    end
+  end
 end
