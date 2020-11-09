d81cdad6d367a11d58e1cab0a7b73c74332f96fd
Connor Ferguson <cpfergus1@gmail.com>
2020-11-09 10:23:48 -0700

    implemented model tests for required functionality

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -5,4 +5,31 @@ RSpec.describe Doctor, type: :model do
     it {should have_many(:patients).through(:patients_doctors)}
     it {should belong_to(:hospital)}
   end
+
+  describe 'class methods' do
+    before :each do
+      @hospital = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+      @doctor1 = @hospital.doctors.create!(name: 'Meredeth Grey',
+                                          specialty: 'General Surgery Education',
+                                          education: 'Harvard University')
+
+      @doctor2 = @hospital.doctors.create!(name: 'Alex Karev',
+                                          specialty: 'Pediatric Surgery',
+                                          education: 'John Hopkins University')
+
+      @doctor3 = @hospital.doctors.create!(name: 'Miranda Bailey',
+                                          specialty: 'General Surgery Education',
+                                          education: 'Stanford University')
+
+      @doctor4 = @hospital.doctors.create!(name: 'Derek McDreamy Shepherd',
+                                          specialty: 'Attending Surgeon',
+                                          education: 'University of Pennsylvania')
+
+      @doctor5 = @hospital.doctors.create!(name: 'Mega Doctor',
+                                          specialty: 'Super Surgeon',
+                                          education: 'Harvard University')
+    end
+    it '.unique_universities'
+      expect(Doctor.unique_universities).to eq([])
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,32 @@ RSpec.describe Hospital, type: :model do
   describe "Relations" do
     it {should have_many(:doctors)}
   end
+
+  describe "instance methods" do
+    before :each do
+      @hospital = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+      @doctor1 = @hospital.doctors.create!(name: 'Meredeth Grey',
+                                          specialty: 'General Surgery Education',
+                                          education: 'Harvard University')
+
+      @doctor2 = @hospital.doctors.create!(name: 'Alex Karev',
+                                          specialty: 'Pediatric Surgery',
+                                          education: 'John Hopkins University')
+
+      @doctor3 = @hospital.doctors.create!(name: 'Miranda Bailey',
+                                          specialty: 'General Surgery Education',
+                                          education: 'Stanford University')
+
+      @doctor4 = @hospital.doctors.create!(name: 'Derek McDreamy Shepherd',
+                                          specialty: 'Attending Surgeon',
+                                          education: 'University of Pennsylvania')
+
+      @doctor5 = @hospital.doctors.create!(name: 'Mega Doctor',
+                                          specialty: 'Super Surgeon',
+                                          education: 'Harvard University')
+    end
+    it ".doctors_count" do
+      expect(@hospital.doctors_count).to eq(5)
+    end
+  end
 end
