acf41819a224b9875e2ee0cce019e5b05274fd8c
Connor Ferguson <cpfergus1@gmail.com>
2020-11-09 11:10:28 -0700

    created test for extension

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  before :each do
+    @patient_1 = Patient.create!(name: 'Katie Bryce',
+                                  age: 24)
+    @patient_2 = Patient.create!(name: 'Denny Duquette',
+                                  age: 39)
+    @patient_3 = Patient.create!(name: 'Rebecca Pope',
+                                  age: 32)
+  end
+  describe 'when I visit the patient index page' do
+    it 'I see the names of all the patients listed from oldest to yougest' do
+      visit patient_path
+      expect(page.all('li')[0]).to have_content("#{@patient_1.name}, #{@patient_1.age}")
+      expect(page.all('li')[1]).to have_content("#{@patient_2.name}, #{@patient_2.age}")
+      expect(page.all('li')[2]).to have_content("#{@patient_3.name}, #{@patient_3.age}")
+    end
+  end
+end
